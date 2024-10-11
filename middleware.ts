45d871b0a24ccba085baf039af818777c6ee32bd
Ritik <114500316+RitikWeb22@users.noreply.github.com>
2024-10-11 11:17:43 +0530

    Your descriptive commit message here

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,7 @@
-import { auth, clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server'
+import { auth, clerkMiddleware } from '@clerk/nextjs/server'
 
-const protectedRoutes = createRouteMatcher([
-    '/',
-    '/upcoming',
-   '/previous',
-   '/recordings',
-   '/personal-room',
-   '/meetings(.*)'
-])
-export default clerkMiddleware((auth,req)=>{
-    if(protectedRoutes(req)) auth().protect()
-
-    
+export default clerkMiddleware((auth, req) => {
+    auth().protect()
 })
 
 export const config = {
